c50b21b069886d6c8c6eaddaf39b06a2b6730884
markotot <tot.marko@hotmail.com>
2020-04-30 17:45:17 +0200

    Updated play_test
    enable cheat

--- drqn_tf_v1/play_test.py
+++ drqn_tf_v1/play_test.py
@@ -27,6 +27,8 @@ def choose_action():
                 action = 2
             if event.key == pygame.K_RIGHT:
                 action = 3
+            if event.key == pygame.K_SPACE:
+                action = 99
     return action
 
 
@@ -37,6 +39,7 @@ state = env.reset()
 
 display_width = 1024
 display_height = 768
+cheat = False
 
 pygame.init()
 screen = pygame.display.set_mode([display_width, display_height])
@@ -44,36 +47,38 @@ screen = pygame.display.set_mode([display_width, display_height])
 
 max_ep_length = 50
 episode_reward = 0
-num_of_red = 0
-num_of_green = 0
-num_of_stuck = 0
 current_step = 0
-
 
 while current_step < max_ep_length:
 
-    # have to flip/rotate the image -- dunno why
-    image = pygame.surfarray.make_surface(env.render_env())
-    image = pygame.transform.rotate(image, -90) #rotate counter-clockwise
-    image = pygame.transform.flip(image, True, False) #flip on Horizontal axis
-    image = pygame.transform.scale(image, (display_width, display_height)) #rescale image to display size
-
-    draw_text("Score: {:.2f}".format(episode_reward), image, 10, 10)
-    screen.blit(image, (0, 0))
-
+    reward = 0
     action = choose_action()
+
     if action == -1:
         break
-
-    reward = 0
-    if action is not None:
-        current_step += 1
+    elif action == 99:
+        cheat = not cheat
+    elif action is not None:
         next_state, reward, done = env.step(action)
+        current_step += 1
+
+    if cheat:
+        image = pygame.surfarray.make_surface(env.render_full_env())
+    else:
+        image = pygame.surfarray.make_surface(env.render_env())
+
+    # we have to transform the image -- dunno why
+    image = pygame.transform.rotate(image, -90)  # rotate counter-clockwise
+    image = pygame.transform.flip(image, True, False)  # flip on Horizontal axis
+    image = pygame.transform.scale(image, (display_width, display_height))  # rescale image to display size
+    draw_text("Score: {:.2f}".format(episode_reward), image, 10, 10)
+    screen.blit(image, (0, 0))
 
     pygame.display.update()
     episode_reward += reward
 
 
+#Score screen
 pygame.display.get_surface().fill(BLACK)
 draw_text("Score: {:.2f}".format(episode_reward), pygame.display.get_surface(), 10, 10, font_size=128, font_color=WHITE)
 pygame.display.update()
